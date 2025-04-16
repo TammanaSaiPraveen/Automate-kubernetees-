c97b38af1016054187c9a978080da1082847938b
TammanaSaiPraveen <168283772+TammanaSaiPraveen@users.noreply.github.com>
2025-04-16 05:46:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
-app.get("/", (req, res) => res.send("Hello from Kubernetes!"));
-app.listen(3000, () => console.log("App listening on port 3000"));
+app.get("/", (req, res) =>
+  res.send("Hello from Kubernetes!")
+);
+app.listen(3000, () => 
+  console.log("App listening on port 3000")
+);
